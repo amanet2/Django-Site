7bc80fe5bc73dd049fd52871d97cd4374c81acc0
Anthony Manetti <tmanetti1992@gmail.com>
2017-05-10 18:54:03 -0500

    playing around

--- basesite/scenebuilder/views.py
+++ basesite/scenebuilder/views.py
@@ -10,15 +10,10 @@ class IndexView(generic.ListView):
     model = template.MapDoc.scene_grid0
     model1 = template.MapDoc.scene_grid1
     model2 = template.MapDoc.scene_grid2
-
-    i = 0
-    for line in model:
-        if i != 0 and i%30 == 0:
-            line += '\n'
-            print('yes')
+    scene_dict = template.MapDoc.new_map.obj_codes
 
     template_name = 'scenebuilder/index.html'
     context_object_name = 'scene_template'
 
     def get_queryset(self):
-        return [self.model,self.model1,self.model2]
+        return [self.scene_dict,self.model,self.model1,self.model2]
